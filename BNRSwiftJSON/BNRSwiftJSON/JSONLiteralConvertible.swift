7ceb45c66404ae3d19ee9b7575c5da2466df3f3d
Zachary Waldowski <zach@waldowski.me>
2015-08-03 01:38:05 -0400

    Adds cast-style inits, test coverage for all literals

--- BNRSwiftJSON/BNRSwiftJSON/JSONLiteralConvertible.swift
+++ BNRSwiftJSON/BNRSwiftJSON/JSONLiteralConvertible.swift
@@ -10,8 +10,12 @@
 
 extension JSON: ArrayLiteralConvertible {
     
+    public init<Collection: CollectionType where Collection.Generator.Element == JSON>(_ collection: Collection) {
+        self = .Array(Swift.Array(collection))
+    }
+
     public init(arrayLiteral elements: JSON...) {
-        self = .Array(elements)
+        self.init(elements)
     }
     
 }
@@ -20,50 +24,66 @@ extension JSON: ArrayLiteralConvertible {
 
 extension JSON: DictionaryLiteralConvertible {
     
-    public init(dictionaryLiteral pairs: (Swift.String, JSON)...) {
-        var dictionary = Swift.Dictionary<Swift.String, JSON>(minimumCapacity: pairs.count)
+    public init<Dictionary: SequenceType where Dictionary.Generator.Element == (Swift.String, JSON)>(_ pairs: Dictionary) {
+        var dictionary = Swift.Dictionary<Swift.String, JSON>(minimumCapacity: underestimateCount(pairs))
         for (key, value) in pairs {
             dictionary[key] = value
         }
         self = .Dictionary(dictionary)
     }
     
+    public init(dictionaryLiteral pairs: (Swift.String, JSON)...) {
+        self.init(pairs)
+    }
+
 }
 
 // MARK: - FloatLiteralConvertible
 
 extension JSON: FloatLiteralConvertible {
     
-    public init(floatLiteral value: FloatLiteralType) {
+    public init(_ value: Swift.Double) {
         self = .Double(value)
     }
     
+    public init(floatLiteral value: Swift.Double) {
+        self.init(value)
+    }
+
 }
 
 // MARK: - IntegerLiteralConvertible
 
 extension JSON: IntegerLiteralConvertible {
     
-    public init(integerLiteral value: IntegerLiteralType) {
+    public init(_ value: Swift.Int) {
         self = .Int(value)
     }
     
+    public init(integerLiteral value: Swift.Int) {
+        self.init(value)
+    }
+
 }
 
 // MARK: - StringLiteralConvertible
 
 extension JSON: StringLiteralConvertible {
     
+    public init(_ text: Swift.String) {
+        self = .String(text)
+    }
+
     public init(stringLiteral value: StringLiteralType) {
-        self = .String(value)
+        self.init(value)
     }
     
     public init(extendedGraphemeClusterLiteral value: StringLiteralType) {
-        self = .String(value)
+        self.init(value)
     }
     
     public init(unicodeScalarLiteral value: StringLiteralType) {
-        self = .String(value)
+        self.init(value)
     }
     
 }
@@ -71,15 +91,23 @@ extension JSON: StringLiteralConvertible {
 // MARK: - BooleanLiteralConvertible
 
 extension JSON: BooleanLiteralConvertible {
-    public init(booleanLiteral value: Swift.Bool) {
+
+    public init(_ value: Swift.Bool) {
         self = .Bool(value)
     }
+
+    public init(booleanLiteral value: Swift.Bool) {
+        self.init(value)
+    }
+
 }
 
 // MARK: - NilLiteralConvertible
 
 extension JSON: NilLiteralConvertible {
+
     public init(nilLiteral: ()) {
         self = .Null
     }
+
 }
