2dfc6f47ebd0f4b07a448fa36e99e768a12d2441
Tom Comer <tom.comer@gmail.com>
2016-08-16 18:36:33 -0600

    - more swift 6 beta 6 cleanup

--- Sources/JSON.swift
+++ Sources/JSON.swift
@@ -85,9 +85,9 @@ extension JSON: CustomStringConvertible {
         case .Array(let arr):       return Swift.String(describing:arr)
         case .Dictionary(let dict): return Swift.String(describing:dict)
         case .String(let string):   return string
-        case .Double(let double):   return Swift.String(double)
-        case .Int(let int):         return Swift.String(int)
-        case .Bool(let bool):       return Swift.String(bool)
+        case .Double(let double):   return Swift.String(describing:double)
+        case .Int(let int):         return Swift.String(describing:int)
+        case .Bool(let bool):       return Swift.String(describing:bool)
         case .null:                 return "null"
         }
     }
