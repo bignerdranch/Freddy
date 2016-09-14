6ad9d1d33b26d039bfc93942ffd455c37a135310
davidahouse <davidahouse@gmail.com>
2016-09-14 08:43:30 -0400

    New scripts folder

---
+++ Resources/scripts/publish_docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo -e "Generating Jazzy output \n"
+    jazzy --swift-version 2.2 -m Freddy -g "https://github.com/bignerdranch/Freddy" -a "Big Nerd Ranch" -u "https://github.com/bignerdranch" --module-version=2.1.0 -r "http://bignerdranch.github.io/Freddy/"
+
+    echo -e "Moving into docs directory \n"
+    pushd docs
+
+    echo -e "Creating gh-pages repo \n"
+    git init
+    git config user.email "travis@travis-ci.org"
+    git config user.name "travis-ci"
+
+    echo -e "Adding new docs \n"
+    git add -A
+    git commit -m "Publish docs from successful Travis build of $TRAVIS_COMMIT"
+    git push --force --quiet "https://${GITHUB_ACCESS_TOKEN}@github.com/bignerdranch/Freddy" master:gh-pages > /dev/null 2>&1
+    echo -e "Published latest docs.\n"
+
+    echo -e "Moving out of docs clone and cleaning up"
+    popd
+fi
