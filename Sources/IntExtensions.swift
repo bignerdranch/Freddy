b4132f698b286e64a0d230bd910862884aa529d5
Vladimir <volodg@mail.ru>
2017-06-02 23:03:51 +0800

    reduce code duplications and parametrization

---
+++ Sources/IntExtensions.swift
@@ -0,0 +1,37 @@
+//
+//  IntExtensions.swift
+//  Freddy
+//
+//  Created by Volodymyr  Gorbenko on 2/6/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+#if swift(>=4.0)
+#else
+
+public enum ArithmeticOverflow {
+
+  public init(_ overflow: Bool) {
+    self = overflow ? .overflow : .none
+  }
+
+  case none
+  case overflow
+}
+
+extension Int {
+
+  func multipliedReportingOverflow(by other: Int) -> (partialValue: Int, overflow: ArithmeticOverflow) {
+    let (exponent, overflow) = Int.multiplyWithOverflow(self, other)
+    return (exponent, ArithmeticOverflow(overflow))
+  }
+
+  func addingReportingOverflow(_ other: Int) -> (partialValue: Int, overflow: ArithmeticOverflow) {
+    let (exponent, overflow) = Int.addWithOverflow(self, other)
+    return (exponent, ArithmeticOverflow(overflow))
+  }
+}
+
+#endif
--- Sources/JSONParser.swift
+++ Sources/JSONParser.swift
@@ -456,7 +456,6 @@ public struct JSONParser {
 
             case .preDecimalDigits:
                 try parser.parsePreDecimalDigits { c in
-                    #if swift(>=4.0)
                     guard case let (exponent, .none) = 10.multipliedReportingOverflow(by: value) else {
                         throw InternalError.numberOverflow(offset: parser.start)
                     }
@@ -464,15 +463,6 @@ public struct JSONParser {
                     guard case let (newValue, .none) = exponent.addingReportingOverflow(Int(c - Literal.zero)) else {
                         throw InternalError.numberOverflow(offset: parser.start)
                     }
-                    #else
-                    guard case let (exponent, false) = Int.multiplyWithOverflow(10, value) else {
-                        throw InternalError.numberOverflow(offset: parser.start)
-                    }
-                    
-                    guard case let (newValue, false) = Int.addWithOverflow(exponent, Int(c - Literal.zero)) else {
-                        throw InternalError.numberOverflow(offset: parser.start)
-                    }
-                    #endif
                     
                     value = newValue
                 }
@@ -490,15 +480,9 @@ public struct JSONParser {
             }
         }
 
-        #if swift(>=4.0)
         guard case let (signedValue, .none) = sign.rawValue.multipliedReportingOverflow(by: value) else {
             throw InternalError.numberOverflow(offset: parser.start)
         }
-        #else
-        guard case let (signedValue, false) = Int.multiplyWithOverflow(sign.rawValue, value) else {
-            throw InternalError.numberOverflow(offset: parser.start)
-        }
-        #endif
 
         loc = parser.loc
         return .int(signedValue)
